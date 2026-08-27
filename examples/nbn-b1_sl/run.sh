7369912e17fd87c8a7d02a285e00ec7083df5681
Danylo Radevych <danylo.radevych@outlook.com>
2026-08-27 13:03:43 -0400

    nbn-b1_sl example updated

--- examples/nbn-b1_sl/run.sh
+++ examples/nbn-b1_sl/run.sh
@@ -12,8 +12,8 @@ ECHO=echo
 lrun_scf=true
 lrun_rmta=true
 
-lsave_tmp_dir=true
-lsave_tmp_dir_tar=true
+lsave_tmp_dir=false
+lsave_tmp_dir_tar=false
 
 PREFIX='nbn-b1'
 IBRAV=2
@@ -236,7 +236,7 @@ cat > ${NAME}.in << EOF
 &rmta
   prefix = '$PREFIX'
   outdir = '$TMP_DIR'
-  lwrite_dat = .true.
+  lwrite_dat = .false.
   lrmt = .true.
   rmt(1) = $RMT1
   rmt(2) = $RMT2
