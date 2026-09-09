a27d8c8c684a3e868a0ccfbfa909709a054a58a7
Danylo Radevych <danylo.radevych@outlook.com>
2026-09-09 14:08:55 -0400

    bnyru example

---
+++ random_tests/bnyru/run.sh
@@ -0,0 +1,283 @@
+#!/bin/bash
+
+. ../../paths.sh
+
+ncpu=8
+# ncpu=$SLURM_NTASKS
+
+EXEC="mpirun"
+ECHO=echo
+
+lrun_scf=false
+lrun_rmta=true
+
+lsave_tmp_dir=true
+lsave_tmp_dir_tar=true
+
+PREFIX='bnyru'
+IBRAV=0
+! ALAT=
+NAT=8
+NTYP=4
+ECUT=80
+CHARGE=0
+PRESS=0
+OCCUPATIONS="smearing"
+SMEARING='mp'
+DEGAUSS=0.01
+PSEUDO_DIR='pseudo'
+TMP_DIR='tempdir'
+OUT_DIR='output'
+
+k=6
+nk1=$k
+nk2=$k
+nk3=$k
+
+# species.in -------------------------------------------------------------------
+cat > species.in << EOF
+ATOMIC_SPECIES
+  Y      88.905840 Y.upf
+  B      10.810000 B.upf
+  Ru    101.070000 Ru.upf
+  N      14.007000 N.upf
+
+CELL_PARAMETERS angstrom
+        3.586771500000        0.000000000000        0.000000000000
+        0.000000000000        3.586771500000        0.000000000000
+        0.000000000000        0.000000000000        7.701373560000
+
+ATOMIC_POSITIONS crystal
+Y    0.0000000000000000  0.5000000000000000  0.8397613200000003
+Y    0.5000000000000000  0.0000000000000000  0.1602386900000000
+B    0.0000000000000000  0.5000000000000000  0.3297200700000001
+B    0.5000000000000000  0.0000000000000000  0.6702799400000002
+Ru   0.0000000000000000  0.0000000000000000  0.5000000000000000
+Ru   0.5000000000000000  0.5000000000000000  0.5000000000000000
+N    0.5000000000000000  0.0000000000000000  0.8625019100000002
+N    0.0000000000000000  0.5000000000000000  0.1374981000000000
+
+
+
+EOF
+
+
+lfail=false
+
+OCP="-rv"
+ORM="-vrf"
+OMV="-v"
+OTAR="-Jcvf"
+ORSYNC="-avz --delete"
+
+file_check() {
+  if [[ -f "$1" ]]; then
+    $ECHO "$1 exists."
+  else
+    $ECHO "Error: $1 does not exist."
+    lfail=true
+  fi
+}
+
+dir_check() {
+  if [[ -d "$1" ]]; then
+    $ECHO "$1 exists."
+  else
+    $ECHO "Error: $1 does not exist."
+    lfail=true
+  fi
+}
+
+ngauss=1
+if [[ "$SMEARING" == "mp" ]]
+then
+  ngauss=1
+elif [[ "$SMEARING" == "gauss" ]]
+then
+  ngauss=0
+elif [[ "$SMEARING" == "fd" ]]
+then
+  ngauss="-99"
+fi
+
+$ECHO "smearing: " $SMEARING " k: " $k
+
+
+
+
+#. clean.sh
+mkdir -p $OUT_DIR
+mkdir -p $TMP_DIR
+
+# PREPARE ======================================================================
+
+# check input ------------------------------------------------------------------
+$ECHO "lrun_scf = " $lrun_scf
+$ECHO "lrun_rmta = " $lrun_rmta
+$ECHO "ncpu = " $ncpu
+$ECHO "occupations = " $OCCUPATIONS
+$ECHO "smearing = " $SMEARING
+$ECHO "degauss = " $DEGAUSS
+$ECHO "ngauss = " $ngauss
+$ECHO "k = " ${k}
+$ECHO "IBRAV = " $IBRAV
+$ECHO "ALAT = " $ALAT
+$ECHO "ECUT = " $ECUT
+$ECHO "NAT = " $NAT
+$ECHO "NTYP = " $NTYP
+$ECHO ""
+
+# alat.in ----------------------------------------------------------------------
+echo $ALAT > alat.in
+
+
+
+# SCF ==========================================================================
+if [[ "$lrun_scf" == "true" && "$lfail" != "true" ]]
+then
+TASK='scf'
+SUFFIX=$TASK
+
+$ECHO ""
+$ECHO "$SUFFIX"
+$ECHO "-------------->"
+
+NAME=$PREFIX.$TASK
+cat > $NAME.in << EOF
+&control
+ calculation = '$TASK',
+ restart_mode = 'from_scratch',
+ prefix = '$PREFIX',
+ tprnfor = .true.,
+ tstress =.true.,
+ pseudo_dir = '$PSEUDO_DIR',
+ outdir = '$TMP_DIR'
+ etot_conv_thr = 1.0d-5
+ forc_conv_thr = 1.0d-4
+/
+
+&system
+ ibrav = $IBRAV
+ ! celldm(1) = $ALAT
+ nat = $NAT
+ ntyp = $NTYP
+ ecutwfc = $ECUT
+ occupations = '$OCCUPATIONS'
+ degauss = $DEGAUSS
+ smearing = '$SMEARING'
+/
+
+&electrons
+ diagonalization = 'david'
+ mixing_mode = 'plain'
+ mixing_beta = 0.7
+ conv_thr =  1.0d-12
+/
+
+&cell
+  press = $PRESS ! kbar
+/
+
+K_POINTS AUTOMATIC
+$nk1 $nk2 $nk3 0 0 0
+
+EOF
+
+name_check="species.in"
+file_check $name_check
+cat $name_check >> $NAME.in
+
+$ECHO "  running the scf calculation for $PREFIX..."
+$EXEC -n $ncpu $QE_ROOT/bin/pw.x < $NAME.in > $OUT_DIR/$NAME.out
+$ECHO "$SUFFIX is done"
+
+if [ $? -ne 0 ]; then
+  $ECHO “Error: Failed $SUFFIX.”
+  lfail=true
+fi
+
+mkdir -p tempdir_$SUFFIX
+rsync $ORSYNC tempdir tempdir_$SUFFIX/
+
+if [[ $lsave_tmp_dir_tar == "true" ]]
+then
+  XZ_OPT="-T${ncpu}" tar $OTAR ${SUFFIX}_out.tar.xz $TMP_DIR
+fi
+
+grep "Fermi" output/$NAME.out \
+| sed -e 's/the Fermi energy is//g' \
+| sed -e 's/ev//g' > efermi.in
+
+
+fi
+
+
+
+
+
+# RMTA =========================================================================
+if [[ "$lrun_rmta" == "true" && "$lfail" != "true" ]]
+then
+TASK='rmta'
+SUFFIX=$TASK
+
+$ECHO ""
+$ECHO "$SUFFIX"
+$ECHO "-------------->"
+
+name_check="${TMP_DIR}_scf/${TMP_DIR}"
+if [[ -d $name_check ]]
+then
+dir_check $name_check
+rsync $ORSYNC $name_check .
+else
+name_check=scf_out.tar.xz
+file_check $name_check
+rm $ORM ${TMP_DIR}
+tar -xvf $name_check
+mkdir -p ${TMP_DIR}_scf
+rsync $ORSYNC ${TMP_DIR} ${TMP_DIR}_scf/
+fi
+
+
+NAME=$PREFIX.$SUFFIX
+$ECHO "  running RMTA for $PREFIX..."
+cat > ${NAME}.in << EOF
+&rmta
+  prefix = '$PREFIX'
+  outdir = '$TMP_DIR'
+  lwrite_dat = .true.
+  rmt_method = 'pseudotouching'
+/
+EOF
+$EXEC -n 1 $PPRMTA_ROOT/bin/rmta.x < $NAME.in > $OUT_DIR/$NAME.out
+
+$ECHO "$SUFFIX is done"
+
+if [ $? -ne 0 ]
+then
+  $ECHO “Error: Failed $SUFFIX.”
+  lfail=true
+fi
+
+# rm $ORM $TMP_DIR
+
+fi
+
+
+
+
+# SUMMARY ======================================================================
+if [[ "$lfail" == "true" ]]
+then
+$ECHO "FAIL"
+else
+
+if [[ $lsave_tmp_dir != "true" ]]
+then
+  rm $ORM ${TMP_DIR}
+  rm $ORM ${TMP_DIR}_scf
+fi
+
+$ECHO "DONE"
+fi
